ff7fd61183e887f9b8ad4e0f89c1eeea1172b9bc
nejdl <katharina@nejdl.de>
2024-10-24 21:58:53 +0200

    add: appollo client

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  // uri: 'https://spacex-production.up.railway.app',
+  uri: 'https://spacex-production.up.railway.app/graphql',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
