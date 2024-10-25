880d40ed9b0ade7d5e9b14adc090949ec4b9636b
nejdl <katharina@nejdl.de>
2024-10-25 09:47:08 +0200

    add: sorting 2 (WIP)

--- src/components/LaunchList/LaunchList.tsx
+++ src/components/LaunchList/LaunchList.tsx
@@ -1,40 +1,13 @@
 import { CircularProgress } from '@mui/material';
 import { useGetLaunchesQuery } from '../../generated/graphql';
 import Error from '../UI/Error/Error';
-import { gql, useQuery } from '@apollo/client';
-
-const GET_LAUNCHES = gql`
-  query GetLaunches($limit: Int, $offset: Int, $sort: String, $order: String) {
-    launches(limit: $limit, offset: $offset, sort: $sort, order: $order) {
-      id
-      mission_name
-      launch_date_utc
-      rocket {
-        rocket_name
-      }
-    }
-  }
-`;
 
 const LaunchList: React.FC = () => {
   const limit = 10;
-  //   const sortByField = 'launch_date_utc';
-  // const sortByField = 'mission.name';
-  //   const sortByField = 'Launch.mission_name';
   const sortByField = 'mission_name';
-  //   const sortByField = 'launch_date_utc';
   const direction = 'desc';
-  //   const sort = sortByField + ':' + direction;
-  //   const { data, loading, error } = useGetLaunchesQuery({
-  //     variables: { limit: limit, offset: 0, order: direction, sort: sortByField },
-  //   });
-  const { data, loading, error } = useQuery(GET_LAUNCHES, {
-    variables: {
-      limit: limit,
-      offset: 0,
-      sort: sortByField,
-      order: direction,
-    },
+  const { data, loading, error } = useGetLaunchesQuery({
+    variables: { limit: limit, offset: 0, order: direction, sort: sortByField },
   });
 
   if (loading) return <CircularProgress />;
